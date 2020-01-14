132eadac53ab308ec7f4f6de2de49d10b23ad8d0
nicholasmontgomery <na_montgomery@yahoo.co.uk>
2020-01-14 16:42:45 +0000

    initial commit

---
+++ ruby_functions_practice.rb
@@ -0,0 +1,23 @@
+def return_10()
+  return 10
+end
+
+def add(num1, num2)
+  return num1 + num2
+end
+
+def subtract(num1, num2)
+  return num1 - num2
+end
+
+def multiply(int1, int2)
+  int1 * int2
+end
+
+def divide(thing1, thing2)
+  thing1 / thing2
+end
+
+def length_of_string(string)
+  return string.length()
+end
